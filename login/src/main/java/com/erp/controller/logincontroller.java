1e5e4e45f2c5bdd5a1d9021e6a6c63e4ff67eba0
KurohaV <980262601@qq.com>
2020-04-20 15:57:05 +0800

    rou dan cong ji

--- login/src/main/java/com/erp/controller/logincontroller.java
+++ login/src/main/java/com/erp/controller/logincontroller.java
@@ -56,6 +56,7 @@ public class logincontroller {
             return "login";
         }
         model.addAttribute("list",LoginService.findByName(user.getLogin_id()));
+        System.out.println("啥Bxxd");
         return "index";
     }
    
