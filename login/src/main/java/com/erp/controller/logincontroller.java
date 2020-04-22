3ec9799e2cc1839e19b220debf8aa91bb9f5aa76
KurohaV <980262601@qq.com>
2020-04-22 16:26:19 +0800

    erp.controller

--- login/src/main/java/com/erp/controller/logincontroller.java
+++ login/src/main/java/com/erp/controller/logincontroller.java
@@ -57,6 +57,7 @@ public class logincontroller {
         }
         model.addAttribute("list",LoginService.findByName(user.getLogin_id()));
         model.addAttribute("username",user.getLogin_id());
+        System.out.println("shabixxd");
         return "index";
     }
    
